ac2287ab1722b3571ea783073879b0b0d1a59f3b
Gilad Alboher <gilad1299@gmail.com>
2024-02-12 13:25:16 +0200

    telegram bot

---
+++ main.py
@@ -0,0 +1,5 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, Filters, ContextType, CallbackContext
+TOKEN = '6809563019:AAFb54dp6rSBZZmebqMYE64-kwgMQyHu108'
+BOT_USERNAME: Final = '@IntegratorPortal_bot'
